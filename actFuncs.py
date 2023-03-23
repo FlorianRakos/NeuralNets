ad994e2b951c1fbdeac139c499f70e3a0b17be62
Florian Rakos <58404248+FlorianRakos@users.noreply.github.com>
2023-03-23 22:24:49 +0100

    Activation Softmax

--- actFuncs.py
+++ actFuncs.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 
 class ActivationReLu:
@@ -7,3 +8,10 @@ class ActivationReLu:
     def forward(inputs):
         return np.maximum(0, inputs)
 
+class ActivationSoftmax:
+
+    @staticmethod
+    def forward(inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        return probabilities
